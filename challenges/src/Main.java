3690d4d1dec4cfa6db3d8e795e5f648592dd2863
Karl Leswing <karl.leswing@schrodinger.com>
2015-06-18 14:45:12 -0400

    Implemented JollyJumpers

--- challenges/src/Main.java
+++ challenges/src/Main.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        new JollyJumper(sc).solve();
     }
 }
 
@@ -17,18 +19,48 @@ class JollyJumper {
 
     public void solve() {
         final List<Case> cases = parse(scanner);
+        for (final Case aCase: cases) {
+            solve(aCase);
+        }
+    }
 
+    private void solve(final Case aCase) {
+        System.out.println(aCase.solve());
     }
 
     private List<Case> parse(final Scanner scanner) {
         final List<Case> cases = new ArrayList<>();
-        final Case aCase = new Case();
+        while(scanner.hasNextInt()) {
+            final Case aCase = new Case(scanner.nextLine());
+            cases.add(aCase);
+        }
         return cases;
     }
 
     class Case {
         int[] hash = new int[3000];
-        String values;
+        String valueString;
+        public Case(final String s) {
+            this.valueString = s;
+        }
+
+        public String solve() {
+            final StringTokenizer st = new StringTokenizer(valueString);
+            int n = Integer.parseInt(st.nextToken());
+            int start = Integer.parseInt(st.nextToken());
+            for (int i = 1; i < n; i++) {
+                int next = Integer.parseInt(st.nextToken());
+                int diff = Math.abs(start - next);
+                hash[diff] = 1;
+                start = next;
+            }
+            for (int i = 1; i < n; i++) {
+                if (hash[i] == 0) {
+                    return "Not jolly";
+                }
+            }
+            return "Jolly";
+        }
     }
 
 }
